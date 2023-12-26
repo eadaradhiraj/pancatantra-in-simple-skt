da52fc36d681cc5b6693ed3a6c6cc39840bf0623
Dhiraj Eadara <eadaradhiraj@gmail.com>
2023-12-26 21:30:29 +0530

    fix: make website

---
+++ story1.js
@@ -0,0 +1,56 @@
+var story1 = {
+  "topic": {
+    "English": "Loss of friends",
+    "Sanskrit": "मित्र-भेदः"
+  },
+  "text": [
+    {
+      "English": "And so we begin our first story",
+      "Sanskrit": "अथ अतः प्रथमा कथा आरभते।"
+    },
+    {
+      "English": "The name of the story is Loss of friends",
+      "Sanskrit": "कथायाः नाम मित्र-भेदः इति।"
+    },
+    {
+      "English": "In brief the story can be retold in two sentences",
+      "Sanskrit": "सङ्क्षिपते वाक्यद्वयेन कथ्यताम्।"
+    },
+    {
+      "English": "In the forest there was a great friendship between a lion and a bull.",
+      "Sanskrit": "वने सिंहस्य वृषस्य मध्ये महान् स्नेहः आसीत्।"
+    },
+    {
+      "English": "Due to malice and out of greed, a jackal destroyed that friendship.",
+      "Sanskrit": "पिशुनतया अतिलोभ-कारणेन च कश्चित् जम्बुकः तं स्नेहं विनष्टवान्।"
+    },
+    {
+      "English": "The story happened as follows.",
+      "Sanskrit": "कथा एवमेव जाता।"
+    },
+    {
+      "English": "The story is as follows.",
+      "Sanskrit": "कथा एवमेव।"
+    },
+    {
+      "English": "There is a city in the south of India Mahilaropyam.",
+      "Sanskrit": "अस्ति दक्षिणापठस्य जनपदे महिलारोप्यं नाम नगरम्।"
+    },
+    {
+      "English": "Here lived the son of a rich merchant.",
+      "Sanskrit": "अत्र कश्चन धनिकस्य वणिजः पुत्रः न्यवसत्।"
+    },
+    {
+      "English": "His name is Vardhamaanaka.",
+      "Sanskrit": "तस्य नाम वर्धमानकः।"
+    },
+    {
+      "English": "He was lying on bed one night.",
+      "Sanskrit": "एकदा रात्रि-समये सः शय्यारूढः आसीत्।"
+    },
+    {
+      "English": "There was a worry in his mind.",
+      "Sanskrit": "तस्य मनसि चिन्ता उत्पन्ना।"
+    }
+  ]
+}
