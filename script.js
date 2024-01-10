300baa3daf89a48003713800b0ecd6ca0db34db9
Dhiraj Eadara <eadaradhiraj@gmail.com>
2024-01-10 17:20:13 +0530

    FIx: convert 2 table

---
+++ script.js
@@ -0,0 +1,11 @@
+var sents = document.getElementsByTagName("p");
+for (var i=0; i < sents.length; i++) {
+  sents[i].onclick = function(){
+    for (var k=0; k < sents.length; k++) { sents[k].style.color = "black"; }
+    let pid = this.getAttribute("pid");
+    let items2color = document.querySelectorAll(`[pid="${pid}"]`);
+    for (var j=0; j < items2color.length; j++) {
+      items2color[j].style.color = "red";
+    }
+  }
+};
